3b361d0a5e37f6aa512fa58c32ad693e57a15ebd
kriger <j.kriger@pucrs.com.br>
2025-05-30 22:04:34 -0300

    first commit

---
+++ src/test/java/ages/pucrs/hackathon/HackathonApplicationTests.java
@@ -0,0 +1,13 @@
+package ages.pucrs.hackathon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackathonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
